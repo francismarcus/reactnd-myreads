acf9a74ac869d19b2273bf6b0b6171a0463a3acb
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-04 23:33:34 +0100

    Feat: Route app.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,7 +3,16 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Route } from 'react-router-dom'
 
+import Main from './Main';
+import Search from './Search';
+
 class BooksApp extends React.Component {
+  render() {
+    return (<div>
+      <Route exact path="/" component={ Main }/>
+      <Route exact path="/search" component={ Search }/>
+    </div>);
+  }
 }
 
 export default BooksApp;
