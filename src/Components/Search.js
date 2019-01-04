208b9c72649a2d64467507124f44b8dd07d9ced9
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-04 23:43:18 +0100

    Fix: Search.js typo

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 
-class Search extends React.Compnent {
+class Search extends React.Component {
   render() {
     return(
       <div className="search-books">
