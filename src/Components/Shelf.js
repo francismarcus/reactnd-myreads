0a36eff9f8e3c30113c4df2670dec7e30cff29fd
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-05 01:48:01 +0100

    Feat: Search function

--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -8,7 +8,7 @@ class Shelf extends React.Component {
         <h2 className="bookshelf-title">{this.props.name}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-
+            {this.props.book.map((book, key) => <Book bookUpdater={this.props.bookUpdater} book={book} key={key}/>)}
         </ol>
       </div>
     </div>);
